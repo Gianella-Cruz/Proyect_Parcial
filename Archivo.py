c5d68c4d920986a742cbc9f8566a107215b6ce93
Gianella-Cruz <gianellacruzlozano@gmail.com>
2025-10-25 12:01:37 -0500

    eliminar y agregar
    Eliminé las pruebas y cree el archivo definitivo

--- Archivo.py
+++
@@ -1,5 +0,0 @@
-x = int(input('Introduce tu edad:'))
-if x >= 18:
-    print('Adelante')
-else:
-    print('Necesitas autorizacion de un adulto')
--- Prueba.py
+++
@@ -1,3 +0,0 @@
-# Prueba 
-print('Hola mundo')
-x = "esTA ES UNA PRUEBA XD"
